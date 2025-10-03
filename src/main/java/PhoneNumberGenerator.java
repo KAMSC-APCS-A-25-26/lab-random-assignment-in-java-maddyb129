e20490f6ba22f7c8ab1027ca9d9c3e4138264849
maddyb129 <maddyball814@gmail.com>
2025-10-03 14:00:03 -0400

    Update PhoneNumberGenerator.java

--- src/main/java/PhoneNumberGenerator.java
+++ src/main/java/PhoneNumberGenerator.java
@@ -6,14 +6,21 @@ public class PhoneNumberGenerator {
         Random rand = new Random();
         // TODO: Generate first three digits (XXX)
         // Constraint: Cannot contain 8 or 9 (digits 0-7 only)
-        
+        int num1 = rand.nextInt(7);
+        int num2 = rand.nextInt(7);
+        int num3 = rand.nextInt(7);
         // TODO: Generate second three digits (XXX) 
         // Constraint: Must be <= 742 (000-742)
-        
+        int num4 = rand.nextInt(7);
+        int num5 = rand.nextInt(7) ;
+        int num6 = rand.nextInt(7);
         // TODO: Generate third three digits (XXX)
         // No constraints (000-999)
-        
+        int num7 = rand.nextInt(7);
+        int num8 = rand.nextInt(7);
+        int num9 = rand.nextInt(7);
         // TODO: Format and display phone number as XXX-XXX-XXX
         // Include the dashes in the output
+        System.out.print(num1 + num2 + num3 + "-" + num4 + num5 + num6 + "-" + num7 + num8 + num9);
     }
 }
